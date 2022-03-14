559ef34cd654f6b490112417891c6cc77daf1af5
Emilio Rivera <emilio.rivera@hotmail.com>
2022-03-14 17:09:53 -0400

    chore(backend-webserver): Create initial configuration for Express webserver
    Contains a few things such as basic linting, typescript config
    and sample code.

---
+++ backend/webserver/src/applicationSettings.ts
@@ -0,0 +1,4 @@
+export interface ApplicationSettings {
+    environment: 'production' | 'development';
+    serverPort: number;
+}
---
+++ backend/webserver/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Response } from "express";
+import dotenv from "dotenv"
+import { ApplicationSettings } from "./applicationSettings";
+
+// TODO: Add more options
+dotenv.config()
+
+const APPLICATION_SETTINGS: ApplicationSettings = {
+    environment: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    serverPort:  parseInt(process.env.SERVER_PORT || '3000'),
+}
+
+const app = express();
+
+app.get('/', (_, res: Response) => {
+    res.json({'test': 'val'})
+});
+
+app.listen(APPLICATION_SETTINGS.serverPort, () => {
+    console.log(`Listening on ${APPLICATION_SETTINGS.serverPort}`)
+});
